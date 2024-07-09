a23f788bc396a5ccb254000cb404a58272d29c4a
05-rahul-vishwakarma <rahul930vishwakarma@gmail.com>
2024-07-09 00:50:43 -0700

    update

--- server/controler/authControler.js
+++ server/controler/authControler.js
@@ -56,7 +56,7 @@ const login = async (req, res) => {
 
             const token = await jwt.sign(tokenData, process.env.JWT_SECRET, { expiresIn: '1d' });
 
-            res.cookie('token', token, { maxAge: 1 * 24 * 60 * 60 * 1000 , sameSite: 'None', secure : false });
+            res.cookie('token', token, { maxAge: 1 * 24 * 60 * 60 * 1000 , sameSite: 'None', secure : true });
              return res.send({
                 message: "successfully login",
                 status: 200,
